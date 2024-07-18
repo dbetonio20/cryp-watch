edaeff21cfe64203dffa7f12077709522e750b8a
Dominique Betonio <dominique@ereflect.com>
2024-07-18 22:54:06 +0800

    18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HomeComponent } from './components/home/components/home.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
@@ -11,31 +11,24 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from 'src/environment/environment';
 import { IonicModule } from '@ionic/angular';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HomeComponent,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-    IonicModule.forRoot({
-      platform: {
-        /** The default `desktop` function returns false for devices with a touchscreen.
-        * This is not always wanted, so this function tests the User Agent instead.
-        **/
-        'desktop': (win) => {
-          const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(win.navigator.userAgent);
-          return !isMobile;
-        }
-      },
-    }),
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        HomeComponent,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        IonicModule.forRoot({
+            platform: {
+                /** The default `desktop` function returns false for devices with a touchscreen.
+                * This is not always wanted, so this function tests the User Agent instead.
+                **/
+                'desktop': (win) => {
+                    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(win.navigator.userAgent);
+                    return !isMobile;
+                }
+            },
+        })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
